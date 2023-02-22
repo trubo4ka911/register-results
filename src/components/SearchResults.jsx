993406487a0e7f7e9fe6811c2bad7abacd47b835
Anna Trubnikova <trubo4ka911@gmail.com>
2023-02-22 11:14:50 +0200

    Update SearchResults.jsx

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -2,24 +2,24 @@ import React, { useState, useEffect } from 'react';
 import SearchForm from './SearchForm';
 import PersonCard from './PersonCard';
 
-const SearchResults = ({ peopleData, searchQuery, setSearchQuery }) => {
-  const [filteredPeopleData, setFilteredPeopleData] = useState(peopleData);
+const SearchResults = ({ peopleData, searchQuery }) => {
+  const [filteredPeopleData, setFilteredPeopleData] = useState(peopleData || []);
 
   useEffect(() => {
+    if (!peopleData) {
+      return;
+    }
+
     const filteredData = peopleData.filter((person) =>
       person.name.toLowerCase().includes(searchQuery.toLowerCase())
     );
     setFilteredPeopleData(filteredData);
   }, [peopleData, searchQuery]);
 
-  const handleSearch = (query) => {
-    setSearchQuery(query);
-  };
-
   return (
     <div>
       <h1>Search Results</h1>
-      <SearchForm handleSearch={handleSearch} />
+      <SearchForm />
       {filteredPeopleData.map((person) => (
         <PersonCard key={person.id} person={person} />
       ))}
