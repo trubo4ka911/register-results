91f35162446ef6fc2585d7b87a3feef0f78bf5ba
Anna Trubnikova <trubo4ka911@gmail.com>
2023-02-22 16:07:57 +0200

    f

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
-
-import SearchForm from "./components/SearchForm";
+import { useState } from "react";
+import { data } from "./data";
 import SearchResults from "./components/SearchResults";
+import SearchForm from "./components/SearchForm";
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
+  const [peopleData, setPeopleData] = useState(data.people);
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
 
   return (
-    <div className="App">
-      <h1>Registration results</h1>
-      <SearchForm handleSearch={setSearchQuery} />
-      <SearchResults searchQuery={searchQuery} />
+    <div>
+      <SearchForm searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      <SearchResults peopleData={data} searchQuery={searchQuery} />
     </div>
   );
 }
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,32 +1,24 @@
 import React, { useState } from 'react';
 
 const SearchForm = ({ handleSearch }) => {
-  const [inputValue, setInputValue] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    handleSearch(inputValue);
+    const [searchValue, setSearchValue] = useState("");
+  
+    const handleInputChange = (e) => {
+      const query = e.target.value;
+    //   setSearchValue(query);
+    //   handleSearch(query);
+    };
+  
+    return (
+      <form>
+        <label htmlFor="search">Search:</label>
+        <input type="text" id="search" onChange={handleInputChange} />
+        <button type="reset" onClick={() => handleSearch("")}>
+          Clear
+        </button>
+      </form>
+    );
   };
-
-  const handleClear = () => {
-    setInputValue('');
-    handleSearch('');
-  };
-
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  return (
-    <form onSubmit={handleSubmit} className="search-form">
-      <label>
-        Search by name:
-        <input type="text" value={inputValue} onChange={handleInputChange} />
-      </label>
-      <button type="submit">Search</button>
-      <button type="button" onClick={handleClear}>Clear</button>
-    </form>
-  );
-};
+  
 
 export default SearchForm;
