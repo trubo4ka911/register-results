813638c88e28e0729cf70d8f130493f04ca0cfce
Anna Trubnikova <trubo4ka911@gmail.com>
2023-02-22 15:44:02 +0200

    Update SearchResults.jsx

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -4,25 +4,29 @@ import PearsonCard from './PersonCard';
 import {data} from '../data';
 
 const SearchResults = ({ searchQuery }) => {
-  const [searchResults, setSearchResults] = useState([]);
-
-  useEffect(() => {
-    const filteredData = data.filter(person => {
-      return person.name.toLowerCase().includes(searchQuery.toLowerCase());
-    });
-    setSearchResults(filteredData);
-  }, [searchQuery]);
-
-  return (
-    <div className="search-results">
-      <h2>Search Results</h2>
-      <div className="cards">
-        {searchResults.map((result, index) => (
-          <PearsonCard key={index} result={result} />
-        ))}
+    const [searchResults, setSearchResults] = useState([]);
+  
+    useEffect(() => {
+      const filteredData = data.filter(person => {
+        return person.name.toLowerCase().includes(searchQuery.toLowerCase());
+      });
+      console.log("filtered data:", filteredData);
+      setSearchResults(filteredData);
+    }, [searchQuery]);
+  
+    console.log("search results:", searchResults);
+  
+    return (
+      <div className="search-results">
+        <h2>Search Results</h2>
+        <div className="cards">
+          {searchResults.map((result, index) => (
+            <PearsonCard key={index} result={result} />
+          ))}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
+  
 
 export default SearchResults;
