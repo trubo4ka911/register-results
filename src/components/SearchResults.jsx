b667a67cbd85d9a23360d217651f7a789762823c
Anna Trubnikova <trubo4ka911@gmail.com>
2023-02-22 16:06:01 +0200

    Update SearchResults.jsx

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,28 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import SearchForm from './SearchForm';
 import PersonCard from './PersonCard';
-import { data } from '../data';
+import {data} from '../data';
 
 const SearchResults = ({ searchQuery }) => {
-  const [searchResults, setSearchResults] = useState([]);
-
-  useEffect(() => {
-    const filteredData = data.filter((person) => {
-      return person.name.toLowerCase().includes(searchQuery.toLowerCase());
-    });
-    setSearchResults(filteredData);
-  }, [searchQuery]);
-
-  return (
-    <div className="search-results">
-      <h2>Search Results</h2>
-      <div className="cards">
-        {searchResults.map((person, index) => (
-          <PersonCard key={index} person={person} />
-        ))}
+    const [searchResults, setSearchResults] = useState([]);
+  
+    useEffect(() => {
+      const filteredData = data.filter(person => {
+        return person.name.toLowerCase().includes(searchQuery.toLowerCase());
+      });
+      console.log("filtered data:", filteredData);
+      setSearchResults(filteredData);
+    }, [searchQuery]);
+  
+    console.log("search results:", searchResults);
+  
+    return (
+      <div className="search-results">
+        <h2>Search Results</h2>
+        <div className="cards">
+          {searchResults.map((result, index) => (
+            <PersonCard key={index} result={result} />
+          ))}
+        </div>
       </div>
-    </div>
-  );
-};
-
+    );
+  };
+  
 export default SearchResults;
