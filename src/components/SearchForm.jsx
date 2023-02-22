75d7bd68b0fa83a3866da0c6f77d683bd64d2e95
Anna Trubnikova <trubo4ka911@gmail.com>
2023-02-22 16:05:52 +0200

    Update SearchForm.jsx

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,24 +1,32 @@
 import React, { useState } from 'react';
 
 const SearchForm = ({ handleSearch }) => {
-    const [searchValue, setSearchValue] = useState("");
-  
-    const handleInputChange = (e) => {
-      const query = e.target.value;
-    //   setSearchValue(query);
-    //   handleSearch(query);
-    };
-  
-    return (
-      <form>
-        <label htmlFor="search">Search:</label>
-        <input type="text" id="search" onChange={handleInputChange} />
-        <button type="reset" onClick={() => handleSearch("")}>
-          Clear
-        </button>
-      </form>
-    );
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleSearch(inputValue);
   };
-  
+
+  const handleClear = () => {
+    setInputValue('');
+    handleSearch('');
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="search-form">
+      <label>
+        Search by name:
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+      </label>
+      <button type="submit">Search</button>
+      <button type="button" onClick={handleClear}>Clear</button>
+    </form>
+  );
+};
 
 export default SearchForm;
