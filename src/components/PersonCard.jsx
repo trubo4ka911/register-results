7dc2a81fe9c5d687e75957da529585d403b4afbb
Anna Trubnikova <trubo4ka911@gmail.com>
2023-02-22 17:53:18 +0200

    Update PersonCard.jsx

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,14 +1,33 @@
+// import React from 'react';
+
+// const PersonCard = ({ person }) => {
+    
+//   return (
+//     <div>
+//       <h2>{person.name? person.name : ""}</h2>
+//       <p>ID: {person.id}</p>
+//       <p>Time: {person.time}</p>
+//     </div>
+//   );
+// };
+
+// export default PersonCard;
+
 import React from 'react';
 
-const PersonCard = ({ person }) => {
-    
+function PersonCard({ person }) {
+  console.log('person:', person);
+  console.log('person.name:', person.name);
+  console.log('person.email:', person.email);
+  console.log('person.phone:', person.phone);
+
   return (
-    <div>
-      <h2>{person.name? person.name : ""}</h2>
-      <p>ID: {person.id}</p>
-      <p>Time: {person.time}</p>
+    <div className="card">
+      <h3>{person.name? person.name : ""}</h3>
+      <p>{person.email}</p>
+      <p>{person.phone}</p>
     </div>
   );
-};
+}
 
 export default PersonCard;
